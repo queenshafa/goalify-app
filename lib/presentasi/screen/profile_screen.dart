6245057172fcfd4cf66e14820edb2c6dc30b6885
Queenshafa <queenshafaraniabilqis@gmail.com>
2025-04-21 08:26:03 +0700

    fix: profile screen

--- lib/presentasi/screen/profile_screen.dart
+++ lib/presentasi/screen/profile_screen.dart
@@ -21,14 +21,15 @@ class ProfileScreen extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              const SizedBox(height: 60),
+              const SizedBox(height: 40),
               const CircleAvatar(
                 radius: 60,
-                backgroundImage: AssetImage('assets/profile.jpg'),
+                backgroundColor: Colors.white24,
+                // backgroundImage: AssetImage('assets/profile.jpg'),
               ),
               const SizedBox(height: 16),
               const Text(
-                'Faiha Wanda Nabilah',
+                'Queenshafa Rania Bilqis',
                 style: TextStyle(
                   fontFamily: 'Gabarito',
                   fontWeight: FontWeight.w600,
@@ -46,32 +47,32 @@ class ProfileScreen extends StatelessWidget {
                   color: Colors.white70,
                 ),
               ),
-              const SizedBox(height: 32),
+              const SizedBox(height: 38),
               Expanded(
                 child: ListView(
                   padding: const EdgeInsets.symmetric(horizontal: 24),
                   children: [
                     _buildProfileTile(
-                      icon: Icons.travel_explore,
+                      icon: Icons.travel_explore_rounded,
                       title: 'Your Journey',
                       subtitle: 'See your achievements and activities.',
                     ),
                     _buildProfileTile(
-                      icon: Icons.emoji_events,
+                      icon: Icons.emoji_events_rounded,
                       title: 'Trophies & Medals',
                       subtitle: 'List of awards you’ve won.',
                     ),
                     _buildProfileTile(
-                      icon: Icons.event_available,
+                      icon: Icons.event_available_rounded,
                       title: 'Events Attended',
                       subtitle: 'Workshops and events you joined.',
                     ),
                     _buildProfileTile(
-                      icon: Icons.card_membership,
+                      icon: Icons.card_membership_rounded,
                       title: 'Certificates',
                       subtitle: 'All your earned certificates.',
                     ),
-                    const SizedBox(height: 20),
+                    const SizedBox(height: 10),
                     _buildEditButton(),
                     const SizedBox(height: 20),
                   ],
@@ -84,18 +85,21 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildProfileTile(
-      {required IconData icon,
-      required String title,
-      required String subtitle}) {
+  Widget _buildProfileTile({
+    required IconData icon,
+    required String title,
+    required String subtitle,
+  }) {
     return Container(
       margin: const EdgeInsets.only(bottom: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 12),
       decoration: BoxDecoration(
-        color: const Color(0xFF2B2890),
-        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.3),
+        border: Border.all(color: Colors.white.withOpacity(0.3), width: 1),
+        borderRadius: BorderRadius.circular(18),
       ),
       child: ListTile(
-        leading: Icon(icon, color: Colors.white),
+        leading: Icon(icon, color: Colors.white, size: 28),
         title: Text(
           title,
           style: const TextStyle(
@@ -106,17 +110,14 @@ class ProfileScreen extends StatelessWidget {
         ),
         subtitle: Text(
           subtitle,
-          style: const TextStyle(
-            fontFamily: 'GabaritoMedium',
-            fontSize: 12,
-            color: Colors.white70,
-          ),
+          style: TextStyle(
+              fontFamily: 'Gabarito',
+              fontSize: 11,
+              color: Colors.white.withOpacity(0.7)),
         ),
-        trailing:
-            const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
-        onTap: () {
-          // aksi ketika diklik (optional)
-        },
+        trailing: const Icon(Icons.arrow_forward_ios_rounded,
+            color: Colors.white, size: 18),
+        onTap: () {},
       ),
     );
   }
@@ -125,10 +126,17 @@ class ProfileScreen extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          )
+        ],
       ),
       child: ListTile(
-        leading: const Icon(Icons.edit, color: Color(0xFF2B2890)),
+        leading: const Icon(Icons.person, color: Color(0xFF2B2890)),
         title: const Text(
           'Edit Profile',
           style: TextStyle(
@@ -138,10 +146,8 @@ class ProfileScreen extends StatelessWidget {
           ),
         ),
         trailing: const Icon(Icons.arrow_forward_ios_rounded,
-            color: Color(0xFF2B2890)),
-        onTap: () {
-          // aksi edit profile
-        },
+            color: Color(0xFF2B2890), size: 18),
+        onTap: () {},
       ),
     );
   }
